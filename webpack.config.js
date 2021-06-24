b759ed106997fce981f2522e8f5a667211c699d4
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-06-24 12:04:57 -0600

    Changed webpack.config to have two objs

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 const ESLintPlugin = require('eslint-webpack-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
+const path = require('path')
+const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 //const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
+const clientConfig = {
+  mode: 'production',
   entry: __dirname + '/client/src/index.jsx',
   module: {
     rules: [
@@ -36,10 +41,14 @@ module.exports = {
 //     hot: true
 //  },
   output: {
-    filename: 'bundle.js',
-    path: __dirname + '/client/dist'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
-  plugins: [new ESLintPlugin()],
+  plugins: [
+    new ESLintPlugin(),
+    new webpack.DefinePlugin({
+    __isBrowser__: "true"
+  })],
   optimization: {
     minimize: true,
     minimizer: [
@@ -49,3 +58,28 @@ module.exports = {
     ]
   }
 };
+
+const serverConfig = {
+  mode: "production",
+  entry: './src/server/index.js',
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new webpack.DefinePlugin({
+      __isBrowser__: "false"
+    }),
+  ]
+};
+
+module.exports = [clientConfig, serverConfig]
