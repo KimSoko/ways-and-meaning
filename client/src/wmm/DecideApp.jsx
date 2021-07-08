79aa1ff2736788936b487a0b12909526ab542075
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-07-07 20:23:34 -0600

    Changed background for decide app

--- client/src/wmm/DecideApp.jsx
+++ client/src/wmm/DecideApp.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import image from '../../dist/media/choices-2.png';
 
 const DecideApp = () => {
   return (
     <div className="two-col-container">
+    <div className="outer-left-container" style={{
+      backgroundImage: 'url(' + image + ')',
+      backgroundSize: 'cover',
+      backgroundPosition: '100%',
+      backgroundRepeat: 'no-repeat'
+    }}>
       <div className="left-container">
         <div className="two-col-container">
           <div className="left-container-nested">
             <h1>What Matters Most</h1>
           </div>
           <div className="inline">
-            <Link to='/'>
+            <Link to="/">
               <button className="home-button">home</button>
             </Link>
           </div>
         </div>
+      </div>
+      <div className="left-container">
         <h3>A decision making app</h3>
+        <div className="inner-text">
+
+        </div>
       </div>
     </div>
+  </div>
   )
 };
 
