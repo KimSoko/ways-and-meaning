d604dae8bf244f2b88728af9745019147447f5c1
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-07-08 17:16:34 -0600

    DecideChoices formatted and styled

--- client/src/wmm/DecideChoices.jsx
+++ client/src/wmm/DecideChoices.jsx
@@ -1,19 +1,20 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react/prop-types */
 import React from 'react';
 
 const DecideChoices = ({ list, handleVote }) => {
 
   return (
-    <div>
+    <div className="left-container no-top-margin">
       {list && (
-        <div>
+        <div className="options-container">
           {list[0].display === 'A' && (
             <>
-              <div key={`${list[0].name}-${list[0].value}`} id="top-div">
+              <div key={`${list[0].name}-${list[0].value}`} id="left-div">
                 <button className="option-button" id="option-A" name={list[0].name} onClick={handleVote}>{list[0].value}</button>
               </div>
-
-              <div key={`${list[1].name}-${list[1].value}`} id="bottom-div">
+              <p className="or">or</p>
+              <div key={`${list[1].name}-${list[1].value}`} id="right-div">
                 <button className="option-button" id="option-B" name={list[1].name} onClick={handleVote}>{list[1].value}</button>
               </div>
             </>
@@ -21,11 +22,11 @@ const DecideChoices = ({ list, handleVote }) => {
 
           {list[0].display === 'B' && (
             <>
-              <div key={`${list[0].name}-${list[0].value}`} id="top-div">
+              <div key={`${list[0].name}-${list[0].value}`} id="left-div">
                 <button className="option-button" id="option-C" name={list[0].name} onClick={handleVote}>{list[0].value}</button>
               </div>
-
-              <div key={`${list[1].name}-${list[1].value}`} id="bottom-div">
+              <p className="or">or</p>
+              <div key={`${list[1].name}-${list[1].value}`} id="right-div">
                 <button className="option-button" id="option-D" name={list[1].name} onClick={handleVote}>{list[1].value}</button>
               </div>
             </>
@@ -33,17 +34,20 @@ const DecideChoices = ({ list, handleVote }) => {
 
           {list[0].display === 'C' && (
             <>
-              <div key={`${list[0].name}-${list[0].value}`} id="top-div">
+              <div key={`${list[0].name}-${list[0].value}`} id="left-div">
                 <button className="option-button" id="option-E" name={list[0].name} onClick={handleVote}>{list[0].value}</button>
               </div>
-
-              <div key={`${list[1].name}-${list[1].value}`} id="bottom-div">
+              <p className="or">or</p>
+              <div key={`${list[1].name}-${list[1].value}`} id="right-div">
                 <button className="option-button" id="option-F" name={list[1].name} onClick={handleVote}>{list[1].value}</button>
               </div>
             </>
           )}
         </div>
       )}
+      <div className="instructions">
+        <p>Click on the option that matters most to you. <strong>You must choose one.</strong> The app will continue to offer new options until all possible combinations have been considered. When you're done, your results will be displayed automatically.</p>
+      </div>
     </div>
   )
 };
