b4abe05a1846c502fef8b7a3f1d88ca3a92c2216
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-07-07 20:05:19 -0600

    Final edits for the night

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -7,23 +7,34 @@ const Error = () => {
   return (
     <div className="two-col-container">
       <div className="outer-left-container" style={{
-          backgroundImage: 'url(' + image + ')',
-          backgroundSize: 'cover',
-          backgroundPosition: '100%',
-          backgroundRepeat: 'no-repeat'
-        }}>
-      <div className="left-container">
-        <h1>Whoops, my bad.</h1>
-        <h3>I messed up and sent you nowhere. Maybe you were trying to...</h3>
-        <div className="with-img">
-          <ul>
-            <Link to='/ideas'><li>Read an article?</li></Link>
-            <Link to='/purpose'><li>Sign up for my free purpose toolkit?</li></Link>
-            <Link to='/app'><li>Get help with a major life decision?</li></Link>
-            <Link to='/about'><li>Learn more about me?</li></Link>
-            <li>Anything else? <a href="mailto:kim.s.schaefer@gmail.com" target="_blank" rel="noreferrer">Email me</a> and let me know what you need</li>
-          </ul>
+        backgroundImage: 'url(' + image + ')',
+        backgroundSize: 'cover',
+        backgroundPosition: '100%',
+        backgroundRepeat: 'no-repeat'
+      }}>
+        <div className="left-container">
+          <div className="two-col-container">
+            <div className="left-container-nested">
+              <h1>Whoops, my bad.</h1>
+            </div>
+            <div className="inline">
+              <Link to="/">
+                <button className="home-button">home</button>
+              </Link>
+            </div>
+          </div>
         </div>
+        <div className="left-container">
+          <h3>I messed up and sent you nowhere. Maybe you were trying to...</h3>
+          <div className="with-img">
+            <ul>
+              <Link to='/ideas'><li>Read an article?</li></Link>
+              <Link to='/purpose'><li>Sign up for my free purpose toolkit?</li></Link>
+              <Link to='/app'><li>Get help with a major life decision?</li></Link>
+              <Link to='/about'><li>Learn more about me?</li></Link>
+              <li>Anything else? <a href="mailto:kim.s.schaefer@gmail.com" target="_blank" rel="noreferrer">Email me</a> and let me know what you need</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
