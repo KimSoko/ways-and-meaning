5688f4739f85d7e1780a33f217ccf849dfa96c2a
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-07-20 17:13:43 -0600

    Enabled nav bars to be sticky on page scroll

--- client/src/ideas/Fear.jsx
+++ client/src/ideas/Fear.jsx
@@ -28,6 +28,48 @@ const Fear = () => {
           <br />
           <h2>My war on fear</h2>
           <p>I was a fearless child. I also lived without challenge, without hardship, without the opportunity to experience failure. As a kid I was outgoing and cute. I made friends easily. I was smart, school was easy for me. Even college was a breeze.</p>
+          <p>My first real challenge in life came when I was volunteering in rural Uganda for a locally run social enterprise in my early 20’s (so typical, I know). They put me in charge of running the ‘business’ aspect of things, but I didn’t really know anything about business.</p>
+          <p>But that’s not what challenged me. That didn’t really even phase me. I was full of confidence at this point in my life. I was challenged because it was my first glimpse of entrepreneurship, the first time I felt a sense of what I should be doing. And frankly, as much as that excited it me, it also scared me.</p>
+          <p>I was in the most remote and culturally different place I’d ever been, and I felt at home. It was very unclear what exactly I was to do with my life, but I knew it had to do with being an entrepreneur. I was learning about my true self.</p>
+          <p>Then I went to business school in Australia which was INCREDIBLY challenging. By far, the most difficult thing I had ever done in my life to that point. But I was determined, I was still full of confidence, I still had never failed at anything I set out to do. I would write a business plan, launch my own social enterprise and live out my dreams!</p>
+          <p>Right!? Wrong. So, so very wrong.</p>
+          <p>I did write an amazing, award-winning business plan. But when I got back to the US to put it into action I became paralzyed by fear.  I had a million excuses, but the biggest of all was that I might fail. And I just couldn’t face that reality.</p>
+          <p><strong>My confidence was shattered by fear.</strong></p>
+          <p>It was my first real failure. The stakes were too high. I had too much riding on my success and I was swallowed up by fear in defense of it all.</p>
+          <p>So instead I discovered depression, and my first experience with unintentional unemployment. There were no jobs. This was not because it was the recession of 2008-2010, but because there was nothing I wanted to do. I wanted to start my own business and that wasn’t a job title I could apply for.</p>
+          <p>I ended up taking a job that sounded awesome on paper but that sucked in reality (sound familiar?). The only thing I did enjoy was creating a proposal to launch a social entrepreneurship project under the company’s social responsibility arm. But I never pushed it through. I was stopped, once again, by fear.</p>
+          <p>In 2015 I did launch my own consulting business, which was a step closer to where I wanted to be. I tried really hard to work with just entrepreneurs, but they couldn’t afford my rates. And the corporations that could, didn’t satisfy my need to work with entrepreneurs.</p>
+          <p>To quote Pink Floyd I had become comfortably numb.</p>
+          <p>On paper I had it all: an amazing husband, a cute little house in my dream neighborhood in a great city, a life full of travel and adventure, a masters degree and a mildly successful consulting business. But I was missing something. I was a bit off path.</p>
+          <p>Then I had kids and everything changed. I woke up. My priorities changed. My motivation to finally, actually, really do this increased. I was determined to finally make this work. I was determined to become a real entrepreneur.</p>
+          <p>And now I’m here. The fear that lit its flame within me more than a decade ago is still with me, of course it’s much stronger now, but I’ve also learned to live with it, to love it actually.  It no longer holds such immense power and control over me.</p>
+          <p>Wanna know what happened? Good, because that’s why we’re really here today :)</p>
+          <br />
+          <h2>What fear has taught me</h2>
+          <p>In my experience living with, researching, and understanding fear is that it shows up once we’ve finally discovered our calling, our true self’s purpose. It’s a signal that we’re on the right path. The stronger it is, the more certain we can be that this is what we’re supposed to do.</p>
+          <p>From a biological evolutionary standpoint, fear exists to protect us. It doesn’t want us to get hurt. And the more we love something, the higher the risk that if we don’t get it, we’ll be more hurt.</p>
+          <p>Fear is our internal risk-aversion mechanism.</p>
+          <p>But avoiding risk doesn’t enable us to grow. We learn, we improve, we get better by overcoming challenges, not by avoiding them. The harder something may be, the more we have to gain from succeeding. The more you love something, the greater the gratification you feel when you finally overcome the fear that was preventing you from having it.</p>
+          <p>Fear lives in the shadow of our heart. It is the physical manifestation of un-met love. When I meditate on my fear, I can feel it, physically, right behind my heart or Anahata chakra. When fear causes me anxiety it causes me stress and pain in my middle back, between the shoulders. I obviously have no scientific evidence to back this up, but my guess is that if you’re very mindful of where your fear lives, physically within your body, you’ll find something similar.</p>
+          <p>Of course sometimes we don’t succeed. We don’t overcome. We fail. But that’s just a natural part of trying. If you let a single failure on your path stop you, then yes, you have failed.</p>
+          <p>But if you can fail, learn, and continue on your path, then you’re not failing, you’re succeeding.</p>
+          <p>Oprah says that, “Failure is just a red flag, a big red detour sign that says ‘turn around, this is not your path, go the other way, try again’” Failure is just information, a lesson we need to learn on our ultimate path.</p>
+          <br/>
+          <h2>Symptoms of fear</h2>
+          <p>I’m  absolutely obsessed with Steven Pressfield’s book The War of Art. It’s the quickest, easiest book you’ll ever read that will have a profound effect on your life.  It might take you a couple hours, less if you’re quick.</p>
+          <p>The main idea is that fear shows up in our life as something he calls Resistance, whenever we’re called to improve our lives in any way. To be creative, to launch a new venture, to build our spiritual beliefs, to do the ‘right’ thing, etc.</p>
+          <p>He goes through a lot of symptoms of fear, but the one that rang the truest for me was procrastination. I could be a professor of procrastination. But after learning more about the science of why we procrastinate I realized that what prevented me from finishing anything wasn’t willpower, but rather fear (and boredom, which is one of fear’s favorite costumes).  It’s sometimes easier to NOT do something, then to do it and risk failure.</p>
+          <p>For you though, it might be perfectionism. That’s fear too. Perfectionism is a confidence killer, it leads to procrastination or not completing things because we believe it’s not good enough or ready yet. Good enough is better than not doing it at all.</p>
+          <p>Or maybe you go the other direction. You’re not rigid, you’re chaotic. You simply lose focus or interest or switch to something more ‘fun.’ That’s just distraction, also caused by fear.</p>
+          <p>Fear gets worse the closer we are to the finish line. You can make it 95% of the way there, and let fear stop you. This is usually when procrastination and distraction take over.  This is when you’re challenged the hardest, when you have to be the most mindful of what’s happening and focus on completing whatever it is you said you would do. This is when you have to just keep going.</p>
+          <br/>
+          <h2>How to live with fear</h2>
+          <p>The goal is not to remove all fear from our life, we need that fear to help keep us on track, to protect us from real danger, to remind us, to challenge us, to help move us forward.</p>
+          <p>The goal is to overcome fear. To become aware of it, to feel it, to understand its purpose, then to relieve it of its control over you. You need to let it rest.</p>
+          <p>So what’s the best way to overcome it? Awareness. Being mindful of your thoughts, meditating on your fear, will help you better understand why your fear exists and what you might be able to do about it. You have to face it, experience it, respect it, and only then can you let it go and move on.</p>
+          <p>For many it helps to write down your fears, or talk to somebody about them. Just don’t ignore them, don’t fight them, they will win. It’s in our biological interest for fear to win. You evolved to let fear succeed.</p>
+          <p>Remind yourself that all of the ups and downs are a normal and necessary part of the journey and congratulate yourself for dealing with them all. Learning to fail is a skill, like all others, it requires practice. The more you fail, the better you get at the recovery, the learning of lessons, the trying again. The more you fail, the faster you’ll succeed.</p>
+          <p className="quote">“Do one thing everyday that scares you.” – Eleanore Roosevelt</p>
         </div>
       </div>
     </div>
