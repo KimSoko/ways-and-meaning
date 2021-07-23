a1a050cac9ee562e26439c881609343b9e408ab3
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-07-23 12:07:25 -0600

    Completed on fear and confidence

--- client/src/ideas/Fear.jsx
+++ client/src/ideas/Fear.jsx
@@ -30,7 +30,7 @@ const Fear = () => {
           <h2>My war on fear</h2>
           <p>I was a fearless child. I also lived without challenge, without hardship, without the opportunity to experience failure. As a kid I was outgoing and cute. I made friends easily. I was smart, school was easy for me. Even college was a breeze.</p>
           <p>My first real challenge in life came when I was volunteering in rural Uganda for a locally run social enterprise in my early 20’s (so typical, I know). They put me in charge of running the ‘business’ aspect of things, but I didn’t really know anything about business.</p>
-          <p>But that’s not what challenged me. That didn’t really even phase me. I was full of confidence at this point in my life. I was challenged because it was my first glimpse of entrepreneurship, the first time I felt a sense of what I should be doing. And frankly, as much as that excited it me, it also scared me.</p>
+          <p>But that’s not what challenged me. That didn’t really even phase me. I was full of confidence at that point in my life. I was challenged because it was my first glimpse of entrepreneurship, the first time I felt a sense of what I should be doing. And frankly, as much as that excited it me, it also terrified me.</p>
           <p>I was in the most remote and culturally different place I’d ever been, and I felt at home. It was very unclear what exactly I was to do with my life, but I knew it had to do with being an entrepreneur. I was learning about my true self.</p>
           <p>Then I went to business school in Australia which was INCREDIBLY challenging. By far, the most difficult thing I had ever done in my life to that point. But I was determined, I was still full of confidence, I still had never failed at anything I set out to do. I would write a business plan, launch my own social enterprise and live out my dreams!</p>
           <p>Right!? Wrong. So, so very wrong.</p>
@@ -47,7 +47,7 @@ const Fear = () => {
           <p>Wanna know what happened? Good, because that’s why we’re really here today :)</p>
           <br />
           <h2>What fear has taught me</h2>
-          <p>In my experience living with, researching, and understanding fear is that it shows up once we’ve finally discovered our calling, our true self’s purpose. It’s a signal that we’re on the right path. The stronger it is, the more certain we can be that this is what we’re supposed to do.</p>
+          <p>In my experience living with, researching, and understanding fear I've learned that it shows up once we’ve finally discovered our calling, our true self’s purpose. It’s a signal that we’re on the right path. The stronger it is, the more certain we can be that this is what we’re supposed to do.</p>
           <p>From a biological evolutionary standpoint, fear exists to protect us. It doesn’t want us to get hurt. And the more we love something, the higher the risk that if we don’t get it, we’ll be more hurt.</p>
           <p>Fear is our internal risk-aversion mechanism.</p>
           <p>But avoiding risk doesn’t enable us to grow. We learn, we improve, we get better by overcoming challenges, not by avoiding them. The harder something may be, the more we have to gain from succeeding. The more you love something, the greater the gratification you feel when you finally overcome the fear that was preventing you from having it.</p>
@@ -55,15 +55,15 @@ const Fear = () => {
           <p>Of course sometimes we don’t succeed. We don’t overcome. We fail. But that’s just a natural part of trying. If you let a single failure on your path stop you, then yes, you have failed.</p>
           <p>But if you can fail, learn, and continue on your path, then you’re not failing, you’re succeeding.</p>
           <p>Oprah says that, “Failure is just a red flag, a big red detour sign that says ‘turn around, this is not your path, go the other way, try again’” Failure is just information, a lesson we need to learn on our ultimate path.</p>
-          <br/>
+          <br />
           <h2>Symptoms of fear</h2>
-          <p>I’m  absolutely obsessed with Steven Pressfield’s book The War of Art. It’s the quickest, easiest book you’ll ever read that will have a profound effect on your life.  It might take you a couple hours, less if you’re quick.</p>
+          <p>I’m  absolutely obsessed with Steven Pressfield’s book <cite>The War of Art</cite>. It’s the quickest, easiest book you’ll ever read that will have a profound effect on your life.  It might take you a couple hours, less if you’re quick.</p>
           <p>The main idea is that fear shows up in our life as something he calls Resistance, whenever we’re called to improve our lives in any way. To be creative, to launch a new venture, to build our spiritual beliefs, to do the ‘right’ thing, etc.</p>
           <p>He goes through a lot of symptoms of fear, but the one that rang the truest for me was procrastination. I could be a professor of procrastination. But after learning more about the science of why we procrastinate I realized that what prevented me from finishing anything wasn’t willpower, but rather fear (and boredom, which is one of fear’s favorite costumes).  It’s sometimes easier to NOT do something, then to do it and risk failure.</p>
           <p>For you though, it might be perfectionism. That’s fear too. Perfectionism is a confidence killer, it leads to procrastination or not completing things because we believe it’s not good enough or ready yet. Good enough is better than not doing it at all.</p>
           <p>Or maybe you go the other direction. You’re not rigid, you’re chaotic. You simply lose focus or interest or switch to something more ‘fun.’ That’s just distraction, also caused by fear.</p>
           <p>Fear gets worse the closer we are to the finish line. You can make it 95% of the way there, and let fear stop you. This is usually when procrastination and distraction take over.  This is when you’re challenged the hardest, when you have to be the most mindful of what’s happening and focus on completing whatever it is you said you would do. This is when you have to just keep going.</p>
-          <br/>
+          <br />
           <h2>How to live with fear</h2>
           <p>The goal is not to remove all fear from our life, we need that fear to help keep us on track, to protect us from real danger, to remind us, to challenge us, to help move us forward.</p>
           <p>The goal is to overcome fear. To become aware of it, to feel it, to understand its purpose, then to relieve it of its control over you. You need to let it rest.</p>
@@ -71,6 +71,63 @@ const Fear = () => {
           <p>For many it helps to write down your fears, or talk to somebody about them. Just don’t ignore them, don’t fight them, they will win. It’s in our biological interest for fear to win. You evolved to let fear succeed.</p>
           <p>Remind yourself that all of the ups and downs are a normal and necessary part of the journey and congratulate yourself for dealing with them all. Learning to fail is a skill, like all others, it requires practice. The more you fail, the better you get at the recovery, the learning of lessons, the trying again. The more you fail, the faster you’ll succeed.</p>
           <p className="quote">“Do one thing everyday that scares you.” – Eleanore Roosevelt</p>
+          <br />
+          <h2>On confidence</h2>
+          <p>Confidence is a critical ingredient in overcoming fear and ultimately finding success. In fact, Cameron Anderson, a psychologist who works in the business school at the University of California at Berkeley and studies confidence has concluded that confidence matters just as much as competence in reaching success.</p>
+          <p>And if you’re a woman you’re at an even worse disadvantage because even more studies show that women tend to have less confidence in themselves than men. For example, when women fail they tend to attribute their failure to an internal attribution as in ‘I knew I wasn’t good enough to do this.’ Men on the other hand tend to blame external attributions such as ‘this is a tough situation for anyone.’</p>
+          <br />
+          <h2>Build your confidence</h2>
+          <p>Confidence is the water that simmers fear. You will never find lasting acceptance of your fear without believing in yourself first. </p>
+          <p>So where do you get belief? From within. External praise and support can help. But belief in yourself has to come from within.</p>
+          <p>I recently read <cite>The Power of Habit</cite> by Charles Duhigg and the last part of changing habits (for good) requires having belief in yourself that you can change.</p>
+          <p>My own lack of confidence in myself is a big reason why I have a lot (but not all) of the bad habits I have, especially my procrastination and inability to follow through with my dreams.</p>
+          <p>Duhigg says that under the most stressful conditions we revert to bad ways if we don’t have belief. This is what I did when I buried that first business plan so long ago.</p>
+          <p>But I started to build my confidence back up. My confidence grew. And one day I just finally decided to do it.</p>
+          <p className="quote">“At some point, the pain of not doing it becomes greater than the pain of doing it.” – Stephen Pressfield</p>
+          <br/>
+          <h2>Progress over perfection</h2>
+          <p>Real, lasting confidence comes from taking actions.</p>
+          <p>It’s more important to start than succeed. Starting is always hard. You won’t be good at something when you start, but you take action, and you make progress, and overtime, you get better and better and better, and then you’re succeeding.</p>
+          <p>Confidence is a belief in one’s ability to succeed, a belief that stimulates action. In turn, taking action bolsters one’s belief in one’s ability to succeed. Confidence comes from the result of achieving something. Of doing something. Of succeeding in any small way.</p>
+          <p>One of the reasons procrastination and perfectionism can be so toxic to success is because they prevent us from moving forward. Psychologists and neuroscientists are beginning to better understand motivation and why progress is so important to success. One of the most surprising things about motivation is that it often comes after starting a new behavior, not before.</p>
+          <p>The Progress Principle states that our brains respond better to small wins than massive accomplishments. When we succeed at something, no matter how small, our brain releases small amounts of dopamine which makes us feel good. Which acts as a sort of reward for taking action, so we take another step, and another.</p>
+          <p>With a small win we get a small dose, with a big win we get a giant surge. But our brain is also quick to return to a ‘normal’ state. It can get used to the level of dopamine and require more and more to have that same great feeling (this is similar to how we increase our tolerance to drugs).</p>
+          <p>So researchers are now finding that it’s better to have lots of small, continuous wins. To simply make progress towards a goal. As opposed to focus all energy and motivation on reaching one giant goal, like winning an Olympic medal. The success from the Olympics will certainly feel better than say posting a new blog. But within a year, that Olympian is likely to be back to the state they were before they won…and then what?</p>
+          <p>Affective forecasting is to blame here. And the solution…is to just enjoy the journey and every tiny step along the way.</p>
+          <br/>
+          <h2>Confidence and planning</h2>
+          <p>Planning helps build confidence because it can attempt to reduce risks, it reduces uncertainty and thus anxiety. Its an easy way to tell your brain to calm down, fear not, we got this. It helps you to believe in yourself.</p>
+          <p>But planning isn’t doing. This can be one of the fallacies of planning (and I’m a self-proclaimed expert planner, building a business around the importance of planning :)</p>
+          <p>Now, that’s not to say you jump into any new venture, project, life phase, etc. without a plan. But you do have to be mindful not to OVER plan. (This is actually my very favorite way to procrastinate, to be honest).</p>
+          <p>Plan just enough so that you can see a path, you can build your confidence, you can focus your mind on the necessary actions to take. But then DO.  You don’t get more confidence with more planning. You just get more procrastination.</p>
+          <p>Also, plan on failing. Because you will. If you commit to a daily meditation practice…define ‘daily’ as ‘most days’ because you know something will come up one day and if missing that day triggers you into feeling failure and quitting, you’ve lost. But if missing that day doesn’t seem like a big deal and you keep going the next day, you’ve won.</p>
+          <br/>
+          <h2>Embrace uncertainty</h2>
+          <p>I’m a recovering control freak which really just means I have a low tolerance for  uncertainty.</p>
+          <p>As a rule, humans prefer certainty to uncertainty. Studies have shown that people would rather definitely get an electric shock now than maybe be shocked later, and show greater nervous-system activation when waiting for an unpredictable shock (or other unpleasant stimulus) than an expected one. Where people differ is in the degree to which uncertainty bothers them.</p>
+          <p>But there are things we can do to cope, to lessen the degree to which uncertainty affects us. Planning is one of those things. Being mindful is another. Living in the present and reducing your worry about future unknowns can help you embrace uncertainty.</p>
+          <p>Building self confidence also helps, especially when it comes to decision making. Having a clear sense of purpose and plan can help with this, but at the end of the day you really have to believe in yourself. </p>
+          <p>As Brene Brown would say in her book <cite>Daring Greatly</cite>, eventually, you just have to let go and be vulnerable.</p>
+          <p className="quote">“I did believe that I could opt out of feeling vulnerable, so when it happened–when the phone rang with unimaginable news; or when I was scared; or when I loved so fiercely that rather than feeling gratitude and joy I could only prepare for loss–I controlled things. I managed situations and micromanaged the people around me. I performed until there was no energy left to feel. I made what was uncertain certain, no matter what the cost. I stayed so busy that the truth of my hurting and my fear could never catch up. I looked brave on the outside and felt scared on the inside. Slowly I learned that this shield was too heavy to lug around, and that the only thing it really did was keep me from knowing myself and letting myself be known.” – Brene Brown</p>
+          <br/>
+          <h2>On creating</h2>
+          <p>I believe that our creativity, or more generally, that the things we create come from within us. They come from our life source, our soul, an energy that we can not physically see or feel. It is our job to keep the widow to that sacred place open and allow what comes from us to be. Fear, lack of confidence and anxiety block that hole.</p>
+          <p>Everyone creates. Creativity does not belong to artists only. It belongs to everyone. Engineers create. Mathamaticians create. Writers create. Lawyers create. Photographers create. Mothers create. Doctors and scientists create.  If you have any vocation, if you spend your day working in any way, you create.</p>
+          <p>I believe that creating is akin to sleeping. It is something we NEED, but something we often ignore or de-prioritize, or even consider non-essential.</p>
+          <p>But it is not up to us to judge our creations. It is only our job to create, to put things out there, and to let society deem the value of what we’ve made. That can be a very hard pill to swallow, especially for control freaks, evidence-based gurus and data junkies. We want to ‘control’ the creative process. But that doesn’t work. We need to just let what’s inside us to flow on out in to the world. We can trust our gut, but we can’t let the critics block our flow.</p>
+          <p>And the biggest critic of all is often ourself. Self-sabotague is real. Self-doubt is real. Self-love sounds awful, doesn’t it? But there’s good news, because it’s not.</p>
+          <p>I discovered compassionate kindness meditation and it’s changed my life. The idea is to focus on something or someone you do truly love, and then try to find that same love in people further from those you typically love, including yourself, and ultimately sending that love to your enemies.</p>
+          <p>Through this practice I began reciting a mantra that has really summarized this entire article into a few simple words: <strong>my belief is greater than my fear</strong>.</p>
+          <p>My belief in myself, that I am capable, that I am on the right path, that this is what I’m meant to do, that I will succeed (eventually) is stronger than my fear of failure. They are true words, that I need to be reminded of constantly.</p>
+          <br/>
+          <h2>In summary</h2>
+          <p>Confidence is what turns thoughts into actions. Fear is what holds us back. We all have a biological human desire to create, to produce, to contribute in some way and we all feel better when we can satisfy that need.</p>
+          <p>But we don’t always succeed.</p>
+          <p>Those who do, believe in themselves. That belief builds confidence, which turns into actions, which makes us feel good.</p>
+          <p>So next time you’re staring at a blank page, or hesitant to hit ‘send’ or ‘publish’ or ‘done’ be mindful of what’s holding you back, feel it, then tell it not to worry. You know what you’re doing, you’re on your path. Even failure is progress and all progress is success.</p>
+          <p>So now I leave you with words from a great man. Theodore Roosevelt’s ‘Man in the Arena’ quote from a speech titled <cite>Citizenship of the Republic</cite> which he gave in France in April 1910:</p>
+          <p className="quote">“It is not the critic who counts; not the man who points out how the strong man stumbles, or where the doer of deeds could have done them better. The credit belongs to the man who is actually in the arena, whose face is marred by dust and sweat and blood; who strives valiantly; who errs, who comes short again and again, because there is no effort without error and shortcoming; but who does actually strive to do the deeds; who knows great enthusiasms, the great devotions; who spends himself in a worthy cause; who at the best knows in the end the triumph of high achievement, and who at the worst, if he fails, at least fails while daring greatly, so that his place shall never be with those cold and timid souls who neither know victory nor defeat.” – Theodore Roosevelt</p>
+          <p>Believe in yourself. Then do what you have to do.</p>
         </div>
       </div>
     </div>
